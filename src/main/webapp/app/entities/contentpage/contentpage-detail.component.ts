93c2d248dee0222103c5748d37c09b17b334d781
echasin <echasin@innvosolutions.com>
2020-12-01 22:20:32 -0800

    WIP Forms and Form Groups View

--- src/main/webapp/app/entities/contentpage/contentpage-detail.component.ts
+++ src/main/webapp/app/entities/contentpage/contentpage-detail.component.ts
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IContentpage } from 'app/shared/model/contentpage.model';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { FormBuilder } from '@angular/forms';
+import { addConsoleHandler } from 'selenium-webdriver/lib/logging';
 
 @Component({
   selector: 'jhi-contentpage-detail',
@@ -10,10 +15,36 @@ import { IContentpage } from 'app/shared/model/contentpage.model';
 export class ContentpageDetailComponent implements OnInit {
   contentpage: IContentpage | null = null;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  // Defines FormGroup
+  viewForm = this.fb.group({
+    id: [],
+    title: [],
+    contenthtml: [],
+  });
+
+  // Added constructor for fb: FormBuilder
+  constructor(protected activatedRoute: ActivatedRoute, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    // Subscribing data to contentpage
     this.activatedRoute.data.subscribe(({ contentpage }) => (this.contentpage = contentpage));
+    // Subscribing data to contentpage using updateFrom function
+    this.activatedRoute.data.subscribe(({ contentpage }) => {
+      this.updateForm(contentpage);
+    });
+  }
+
+  // Populates viewform with data
+  updateForm(contentpage: IContentpage): void {
+    this.viewForm.patchValue({
+      id: contentpage.id,
+      title: contentpage.title,
+      contenthtml: contentpage.contenthtml,
+    });
+    // Getting all Data from viewform
+    console.warn(this.viewForm.value);
+    // Getting Data for title from viewform
+    console.warn(this.viewForm.controls['title'].value);
   }
 
   previousState(): void {
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   imports: [
     RouterModule.forChild([
